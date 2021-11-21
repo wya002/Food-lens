8c1b73f3437a5c08e4f248218f59d47d44aae35f
Goldor <75249093+wya002@users.noreply.github.com>
2021-11-21 11:58:11 +0900

    Update ResultActivity.java

--- app/src/main/java/com/example/tesseracttest/ResultActivity.java
+++ app/src/main/java/com/example/tesseracttest/ResultActivity.java
@@ -2,7 +2,8 @@ package com.example.tesseracttest;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
+//import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -28,6 +31,8 @@ public class ResultActivity extends AppCompatActivity {
     private String datapath = "";
     private String lang = "";
     private ImageView test;
+    private String yesorno = "";
+    private View result_color;
 
     static TessBaseAPI sTess;
 
@@ -44,9 +49,39 @@ public class ResultActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1001);
             }
         });
+        TextView textResult = (TextView) findViewById(R.id.ocr_Result);
+        TextView yesorno = (TextView) findViewById(R.id.ocr_Show);
+        View result_color = (View) findViewById(R.id.result_color);
         Intent myIntent = getIntent();
-        test = (ImageView) findViewById(R.id.scan_test);
-        test.setImageBitmap((Bitmap)myIntent.getParcelableExtra("Captured"));
+        //test = (ImageView) findViewById(R.id.scan_test);
+        //test.setImageBitmap((Bitmap)myIntent.getParcelableExtra("Captured"));
+        ArrayList<String> data = (ArrayList<String>) myIntent.getSerializableExtra("List");
+        ArrayList<String> show_res = new ArrayList<>();
+            if(data.contains("설탕")){
+                show_res.add("설탕");
+            }
+            if(data.contains("복숭아")){
+                show_res.add("복숭아");
+            }
+            if(data.contains("홍차")){
+                show_res.add("홍차");
+            }
+        for(int j = 0; j < show_res.size(); j++) {
+            textResult.append(show_res.get(j));
+            if(j < show_res.size()-1){
+                textResult.append(", ");
+            }
+        }
+        if(show_res.size() == 0){
+            yesorno.setText("알러지 성분이 검출되지 않았습니다!");
+            textResult.append("없음");
+            result_color.setBackgroundColor(Color.parseColor("#BBF070"));
+        }
+        if(show_res.size() > 0){
+            yesorno.setText("알러지 성분이 검출되었습니다.");
+            result_color.setBackgroundColor(Color.parseColor("#F07470"));
+        }
+        //textResult.setText(data.get(0));
 
         sTess = new TessBaseAPI();
         test = (ImageView) findViewById((R.id.scan_test));
@@ -57,7 +92,7 @@ public class ResultActivity extends AppCompatActivity {
         if(checkFile(new File(datapath+"/tessdata"))) {
             sTess.init(datapath, lang);
         };
-        processImage((Bitmap)myIntent.getParcelableExtra("Captured"));
+        //processImage((Bitmap)myIntent.getParcelableExtra("Captured"));
     }
 
     boolean checkFile(File dir)
@@ -113,7 +148,7 @@ public class ResultActivity extends AppCompatActivity {
         return OCRresult;
     }
      */
-
+    /*
     public void processImage(Bitmap bitmap){
         String OCRresult = "";
         sTess.setImage(bitmap);
@@ -124,5 +159,5 @@ public class ResultActivity extends AppCompatActivity {
             textResult.setText("문자가 인식 되지 않았습니다. 다시 촬영해 주세요.");
         }
     }
+     */
 }
-
